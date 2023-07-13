53d9ba70faa3d884cf82cf8babf327c103f50764
Arox39 <amaury.morot39@gmail.com>
2023-07-13 20:57:29 +0200

    finish the possibity container, starting the Cta components

--- src/containers/Possibility/Possibility.container.jsx
+++ src/containers/Possibility/Possibility.container.jsx
@@ -7,10 +7,10 @@ const Possibility = () => {
         <section id="possibility" className="possibility">
             <img src={possibility}/>
             <div className="possibility-container">
-                <h4 className="top subtitle">Request Early Access to Get Started</h4>
+                <h4 className="top-subtitle">Request Early Access to Get Started</h4>
                 <h1 className="gradient-text">The possibilities are beyond your imagination</h1>
                 <p className="description">Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
-                <h4 className="bottom subtitle">Request Early Access to Get Started</h4>
+                <h4 className="bottom-subtitle">Request Early Access to Get Started</h4>
             </div>
         </section>
     )
